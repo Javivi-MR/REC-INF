a4b766d06b038081342bfeecfe2338e9bf56577f
Francisco Javier Molina Rojas <99264371+Javivi-MR@users.noreply.github.com>
2023-11-22 01:44:07 +0100

    Yo diria que goty del año :D

--- Practica3/src/Crawler.java
+++ Practica3/src/Crawler.java
@@ -4,24 +4,31 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.net.URL;
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 /**
- * Clase que implementa un crawler
+ * Clase Crawler
+ * Esta clase se encarga de realizar el crawling de una página web (la pagina objetivo es la de wikipedia en español)
+ * @author Francisco Javier Molina Rojas
+ * Jdk used 21
+ * @since 18/11/2023
  */
 public class Crawler {
     /**
-     * Atributos
+     * Atributos de la clase
+     * urlQueue: Cola de urls a visitar
+     * visitedUrl: Vector de urls visitadas
+     * seedUrl: Url semilla
+     * actualFile: Contador de archivos guardados
+     * esEspaniolaAbsoluta: Expresión regular para urls absolutas de wikipedia en español
+     * esEspaniolaRelativa: Expresión regular para urls relativas de wikipedia en español
      */
     private Queue<String> urlQueue;
     private Vector<String> visitedUrl;
@@ -29,25 +36,48 @@ public class Crawler {
     private static int actualFile = 0;
     private Pattern esEspaniolaAbsoluta = Pattern.compile("https?:\\/\\/es\\.wikipedia\\.org\\/wiki\\/.*");
     private Pattern esEspaniolaRelativa = Pattern.compile("\\/wiki\\/.*");
+    private long maxUrls;
 
     /**
-     * Constructor
-     * @param seedUrl
+     * Constructor de la clase sin maxUrls
+     * @param seedUrl Url de la página objetivo
+     * Inicializa la cola de urls a visitar con la url semilla y el vector de urls visitadas
      */
     public Crawler(String seedUrl) {
         this.urlQueue = new LinkedList<>();
         this.seedUrl = seedUrl;
         this.visitedUrl = new Vector<String>();
         urlQueue.add(seedUrl);
+        this.maxUrls = 100000;
+
+        File file = new File("./log/logs.txt");
+        file.delete();
     }
 
     /**
-     * Visita las urls de la cola
-     * @throws java.io.IOException
+     * Constructor de la clase con maxUrls
+     * @param seedUrl Url de la página objetivo
+     * @param maxUrls Número máximo de urls a visitar
+     * Inicializa la cola de urls a visitar con la url semilla y el vector de urls visitadas
+     */
+    public Crawler(String seedUrl, long maxUrls) {
+        this.urlQueue = new LinkedList<>();
+        this.seedUrl = seedUrl;
+        this.visitedUrl = new Vector<String>();
+        urlQueue.add(seedUrl);
+        this.maxUrls = maxUrls;
+
+        File file = new File("./log/logs.txt");
+        file.delete();
+    }
+
+    /**
+     * Método que realiza el crawling
+     * Este método se encarga de visitar las urls de la cola, guardar el documento, obtener los links y agregarlos a la cola siempre y cuando no hayan sido visitados
+     * @throws java.io.IOException Excepción de entrada y salida
      */
     public void crawl() throws java.io.IOException {
-        while(!urlQueue.isEmpty()){ // Mientras que existan urls que visitar:
-            // visitar la primera url de la cola
+        while(!urlQueue.isEmpty()){
             String actualUrl = urlQueue.poll();
             visitedUrl.add(actualUrl);
             System.out.println("Crawling: " + actualUrl);
@@ -55,38 +85,42 @@ public class Crawler {
             Document doc = Jsoup.connect(actualUrl).get();
             saveDocument(doc);
 
+            logActualUrl(doc);
+
             Vector<String> links = new Vector<String>();
             links = getLinks(doc);
 
             for(int i = 0; i < links.size(); i++){
                 String link = links.get(i);
-                if(!visitedUrl.contains(link)){
+                if(!visitedUrl.contains(link) && (!link.contains(".svg") && !link.contains(".png") && !link.contains(".jpg"))){
                     urlQueue.add(link);
                     visitedUrl.add(link);
                 }
             }
-
-            System.out.println("Links encontrados: " + links.size());
-            System.out.println(links);
+            maxUrls--;
+            if(maxUrls == 0){
+                break;
+            }
         }
     }
 
     /**
-     * Guarda el documento en la carpeta html
-     * @param doc
+     * Método que guarda el documento en un archivo html
+     * Recibe un objeto Document
+     * @param doc Objeto Document
+     * @throws IOException Excepción de entrada y salida
      */
     public void saveDocument(Document doc) throws IOException {
         File file = new File("./data/" + actualFile + ".html");
         BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(file));
         writer.write(doc.toString());
-        actualFile++;
         writer.close();
     }
 
     /**
-     * Obtiene los links de un documento
+     * Método que obtiene los links de un documento, los guarda en un vector y lo devuelve
      * @param doc
-     * @return
+     * @return Vector of String links
      */
     public Vector<String> getLinks(Document doc) {
         Vector<String> links = new Vector<String>();
@@ -111,5 +145,18 @@ public class Crawler {
             }
         }
         return links;
+    }
+
+    /**
+     * Método que guarda en un archivo de texto el numero que identifica a la url visitada junto a esta
+     * @param doc Objeto Document
+     * @throws IOException Excepción de entrada y salida
+     */
+    public void logActualUrl(Document doc) throws IOException {
+        File file = new File("./log/logs.txt");
+        BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(file, true));
+        writer.write(  actualFile + " " + doc.baseUri() + "\n");
+        writer.close();
+        actualFile++;
     }
 }
--- Practica3/src/Main.java
+++ Practica3/src/Main.java
@@ -1,4 +1,14 @@
+/**
+ * Clase Main
+ * Esta clase se encarga de ejecutar el programa
+ */
 public class Main {
+
+    /**
+     * Método main
+     * @param args Argumentos de la línea de comandos - por el momento no se utilizan
+     * Se crea una instancia de la clase Crawler y se ejecuta el método crawl
+     */
     public static void main(String[] args) {
         Crawler crawler = new Crawler("https://www.wikipedia.es/");
         try {
