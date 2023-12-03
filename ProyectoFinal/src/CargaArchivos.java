a1e78fb7d31bc7c256651e3018cf26b13fd7bd65
Francisco Javier Molina Rojas <99264371+Javivi-MR@users.noreply.github.com>
2023-12-03 21:00:07 +0100

    Part 1 done :)

--- ProyectoFinal/src/CargaArchivos.java
+++ ProyectoFinal/src/CargaArchivos.java
@@ -1,16 +1,19 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-
+import opennlp.tools.stemmer.PorterStemmer;
 public class CargaArchivos {
     private  String folder;
     private  File[] files;
     private String[] stopWords;
+    private PorterStemmer stemmer;
+
     public CargaArchivos(String folder) throws IOException {
         this.folder = folder;
         this.files = new File(folder).listFiles();
         File file = new File("./obj/stopword.txt");
         this.stopWords = Files.readString(file.toPath()).split(" ");
+        this.stemmer = new PorterStemmer();
     }
 
     public void TransformarArchivos() throws IOException {
@@ -24,9 +27,17 @@ public class CargaArchivos {
             for(String stopWord : stopWords){
                 // replace only and only if the word is not in the middle of a word
                 contenido = contenido.replaceAll(" " + stopWord + " ", " ");
+                contenido = contenido.replaceAll("^" + stopWord + " ", "");
             }
-            //write the new content to the same file (overwriting)
-            Files.writeString(file.toPath(), contenido);
+            System.out.println(contenido);
+            String[] contenidoPalabras = contenido.split(" ");
+            String contenidoNuevo = "";
+            for(String palabra : contenidoPalabras){
+                contenidoNuevo += stemmer.stem(palabra) + " ";
+            }
+
+            //write the new content into a new file with the same name in directory ./temp1/
+            Files.writeString(new File("./temp1/" + file.getName()).toPath(), contenidoNuevo);
         }
     }
 
